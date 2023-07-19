d0e26a71634746f977ab8be24d47f105496ac1dc
kajal-1999-cloud <130256390+kajal-1999-cloud@users.noreply.github.com>
2023-07-19 10:25:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 // your code here
+const name= document.getElementById("name").value;
+const year= document.getElementById("year").value;
+const button= document.getElementById("button");
+button.addEventListener('click', (e) =>{
+	e.preventDefault();
+	console.log(name, year);
+	document.getElementById("url").innerText= `https://localhost:8080/?name=${name}&year=${year}`
+})
