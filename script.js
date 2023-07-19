38a4a8aed23acb0af5caa86594f911b5ea44471c
kajal-1999-cloud <130256390+kajal-1999-cloud@users.noreply.github.com>
2023-07-19 11:45:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 // your code here
-const name= document.getElementById("name").value;
-const year= document.getElementById("year").value;
-const button= document.getElementById("button");
-button.addEventListener('click', (e) =>{
-	e.preventDefault();
-	console.log(name, year);
-	document.getElementById("url").innerText= `https://localhost:8080/?name=${name}&year=${year}`
-})
+let head= document.getElementById("url");
+let name= document.getElementById("name");
+let year= document.getElementById("year");
+let button= document.getElementById("button");
+ 
+button.addEventListener('click', addEvent);
+
+function addEvent(callback){
+	callback.preventDefault();
+	if(name.value == "" && year.value != ""){
+		head.innerHTML = `https://localhost:8080/?year=${year.value}`
+	}
+	else if(name.value != "" && year.value == ""){
+		head.innerHTML = `https://localhost:8080/?name=${name.value}`
+	}
+		else if(name.value== "" && year.value== ""){
+		head.innerHTML = `https://localhost:8080/?name=${name.value}&year=${year.value}`
+	}
+	else(){
+		head.innerHTML = `https://localhost:8080/`
+	}
+}
